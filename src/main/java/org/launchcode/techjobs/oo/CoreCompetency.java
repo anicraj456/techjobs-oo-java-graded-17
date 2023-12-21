3ee666a7d3dcbb289edf47c4bf980583777a7b78
anicraj456 <anicraj123@gmail.com>
2023-12-20 18:33:15 -0600

    working on it

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -12,9 +12,24 @@ public class CoreCompetency {
         this.id = nextId;
         nextId++;
     }
-
     public CoreCompetency(String value) {
         this();
+        this.value = value;
+    }
+
+    // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
+    //  ONLY a getter for the 'id' field.
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+
         this.value = value;
     }
 
@@ -37,8 +52,5 @@ public class CoreCompetency {
     public int hashCode() {
         return Objects.hash(id);
     }
-
-    // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
-    //  ONLY a getter for the 'id' field.
 
 }
